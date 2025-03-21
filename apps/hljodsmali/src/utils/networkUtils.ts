528a003ff2cf5211deb83a232ff28f3dba6b6e12
kiddi <kristinnroach@gmail.com>
2025-03-21 20:11:01 +0000

    remove hljodsmali from apps, minnka clutter í bili

--- apps/hljodsmali/src/utils/networkUtils.ts
+++
@@ -1,9 +0,0 @@
-// src/utils/networkUtils.js
-export function isOnline() {
-  return navigator.onLine;
-}
-
-export function onNetworkChange(callback: (isOnline: boolean) => void): void {
-  window.addEventListener('online', () => callback(true));
-  window.addEventListener('offline', () => callback(false));
-}
