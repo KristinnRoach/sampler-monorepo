73d67311a9f4efeff33f8cf0d7b10ecb5a350b31
kiddi <kristinnroach@gmail.com>
2025-06-13 14:16:55 +0000

    quick bug fixes:  karplus audio connection, record button state..

--- apps/test-elements/src/main.js
+++ apps/test-elements/src/main.js
@@ -1,71 +1,51 @@
-import van from '@repo/vanjs-core';
+// import van from '@repo/vanjs-core';
 // import { Toggle } from './components/VanToggle.js';
 // import { getAttributesArr } from './utils/log.js';
 import { createDraggable } from 'animejs';
-import { saveState, loadState, debouncedSaveState } from './state.js';
+// import { saveState, loadState, debouncedSaveState } from './state.js';
 import { defineKarplusSynth, defineSampler } from '@repo/audio-components'; // ./components/KarplusElement.js';
+import { createAudiolib } from '@repo/audio-components';
 
 const init = () => {
-  defineSampler();
-  defineKarplusSynth();
+  // const audioContext = new AudioContext();
+  const audiolib = createAudiolib({ autoInit: true })
+    .then(() => {
+      defineSampler();
+      defineKarplusSynth();
+    })
+    .catch((e) =>
+      console.error(
+        `main.js, 
+      create audiolib: ${audiolib},
+      error: ${e}
+       `
+      )
+    );
 
-  const wrapperEl = document.getElementById('node-container');
-  const samplerEl = wrapperEl.querySelector(`.sampler`);
-  const karplusEl = wrapperEl.querySelector('.karplus');
+  // const wrapperEl = document.getElementById('node-container');
+  // const samplerEl = wrapperEl.querySelector(`.sampler`);
+  // const karplusEl = wrapperEl.querySelector('.karplus');
+  // console.info(karplusEl.synth);
 
-  // const loaderEl = playerEl.querySelector(`.loader`);
-  // const recorderEl = playerEl.querySelector('.recorder');
-  // const envelopeEl = playerEl.querySelector('.ampenv');
-  // const loopControllerEl = playerEl.querySelector('.looper');
-  // const offsetControllerEl = playerEl.getElementById(
-  //   'sample-offset-controller-1'
-  // );
+  // samplerEl.addEventListener('sampler-initialized', () => {
+  // // Load saved state
+  // loadState();
 
-  samplerEl.addEventListener('sampleplayer-initialized', () => {
-    // Load saved state
-    loadState();
+  // // Event listeners to save state on changes
+  // document.querySelectorAll('.draggable').forEach((el) => {
+  //   el.addEventListener('mouseup', debouncedSaveState);
+  // });
 
-    // make connections
-    // recorderEl.addEventListener('recorder-initialized', (e) => {
-    //   recorderEl.connect(playerEl);
-    // });
+  // // Event listeners to detect changes
+  // [samplerEl, karplusEl].forEach((el) => {
+  //   if (el) {
+  //     el.addEventListener('change', debouncedSaveState);
+  //   }
+  // });
 
-    // loaderEl.connect(playerEl);
-    // envelopeEl.connect(playerEl);
-    // loopControllerEl.connect(playerEl);
-    // offsetControllerEl.connect(playerEl);
-
-    // defineKarplusSynth();
-
-    // const lpfFreqSlider = createTestFilterSlider(playerEl);
-    // van.add(envelopeEl, lpfFreqSlider);
-
-    // Set up event listeners to save state on changes
-    document.querySelectorAll('.draggable').forEach((el) => {
-      el.addEventListener('mouseup', debouncedSaveState);
-    });
-
-    // Add event listeners to elements to detect changes
-    [
-      samplerEl,
-      karplusEl,
-      // loaderEl,
-      // recorderEl,
-      // envelopeEl,
-      // loopControllerEl,
-      // offsetControllerEl,
-    ].forEach((el) => {
-      if (el) {
-        el.addEventListener('change', debouncedSaveState);
-      }
-    });
-
-    // Save state before page unload
-    window.addEventListener('beforeunload', saveState);
-  });
-
-  // loopControllerEl.setMinimumGap(0.003);
-  // offsetControllerEl.setMinimumGap(0.1);
+  // // Save state before page unload
+  // window.addEventListener('beforeunload', saveState);
+  // });
 
   // create draggables
   const draggables = document.querySelectorAll('.draggable');
@@ -74,11 +54,11 @@ const init = () => {
   document.addEventListener('keydown', (e) => {
     if (e.key === ' ') e.preventDefault();
 
-    if (e.key === 'Escape') {
-      draggables.forEach((draggable) => {
-        draggable.reset();
-      });
-    }
+    // if (e.key === 'Escape') {
+    //   draggables.forEach((draggable) => {
+    //     draggable.reset();
+    //   });
+    // }
   });
 
   console.log('Web Audio Elements app initialized');
@@ -86,25 +66,25 @@ const init = () => {
 
 document.addEventListener('DOMContentLoaded', () => init());
 
-function createTestFilterSlider(playerEl) {
-  /** Test filter sliders */
-  const { input, div, label, span } = van.tags;
+// function createTestFilterSlider(playerEl) {
+//   /** Test filter sliders */
+//   const { input, div, label, span } = van.tags;
 
-  const lfpHz = van.state(18000);
-  van.derive(() => playerEl.player.setLpfCutoff(lfpHz.val));
+//   const lfpHz = van.state(18000);
+//   van.derive(() => playerEl.player.setLpfCutoff(lfpHz.val));
 
-  return div(
-    { style: 'margin-bottom: 20px;' },
-    label('LPF: '),
-    input({
-      type: 'range',
-      min: 20,
-      max: 20000,
-      step: 10,
-      value: () => lfpHz.val,
-      oninput: (e) => (lfpHz.val = parseFloat(e.target.value)),
-      style: 'margin-left: 10px;',
-    }),
-    span({ style: 'margin-left: 10px;' }, () => lfpHz.val + 'Hz')
-  );
-}
+//   return div(
+//     { style: 'margin-bottom: 20px;' },
+//     label('LPF: '),
+//     input({
+//       type: 'range',
+//       min: 20,
+//       max: 20000,
+//       step: 10,
+//       value: () => lfpHz.val,
+//       oninput: (e) => (lfpHz.val = parseFloat(e.target.value)),
+//       style: 'margin-left: 10px;',
+//     }),
+//     span({ style: 'margin-left: 10px;' }, () => lfpHz.val + 'Hz')
+//   );
+// }
