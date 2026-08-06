a3fed437c4cddafe97822ff0bfae3a0ca77f4411
kiddi <kristinnroach@gmail.com>
2026-08-06 14:46:21 +0000

    Remove legacy parameter descriptors

--- packages/audiolib/src/nodes/params/param-types.ts
+++ packages/audiolib/src/nodes/params/param-types.ts
@@ -1,13 +1,1 @@
-import type { AudioParamDescriptor } from '../../types/audio-param-descriptor';
-
-export interface LibParamDescriptor extends AudioParamDescriptor {
-  nodeId: NodeID;
-  dataType: 'number' | 'boolean' | 'enum';
-  defaultValue: any; // Override to make required and allow any type
-
-  step?: number;
-  enumValues?: string[];
-  group?: string;
-}
-
 export type NormalizeOptions = { from: [number, number]; to: [number, number] };
