b2ad0baad80b44c9b95b2cb9772eece58483b522
kiddi <kristinnroach@gmail.com>
2026-08-06 20:33:42 +0000

    Updates

--- assets/processors-Vs0xoDZy.js
+++ assets/processors-Vs0xoDZy.js
@@ -47,6 +47,110 @@ function findClosest(sortedArray, target, direction = "any", getValue = (x) => x
   );
   return sortedArray[index];
 }
+const SAMPLE_PLAYER_WORKLET_AUDIOPARAMS = {
+  masterGain: {
+    name: "masterGain",
+    defaultValue: 1,
+    minValue: 0,
+    maxValue: 2,
+    automationRate: "k-rate"
+  },
+  envGain: {
+    name: "envGain",
+    defaultValue: 0,
+    minValue: 0,
+    maxValue: 1,
+    automationRate: "a-rate"
+  },
+  velocity: {
+    name: "velocity",
+    defaultValue: 100,
+    minValue: 0,
+    maxValue: 127,
+    automationRate: "k-rate"
+  },
+  pan: {
+    name: "pan",
+    defaultValue: 0,
+    minValue: -1,
+    // -1 hard left
+    maxValue: 1,
+    // 1 hard right
+    automationRate: "k-rate"
+  },
+  playbackRate: {
+    name: "playbackRate",
+    defaultValue: 1,
+    minValue: 0.1,
+    maxValue: 24,
+    automationRate: "a-rate"
+  },
+  // NOTE: Time based params use seconds
+  loopStart: {
+    name: "loopStart",
+    defaultValue: 0,
+    minValue: 0,
+    maxValue: 99999,
+    // Max sample length in seconds
+    automationRate: "k-rate"
+  },
+  loopEnd: {
+    name: "loopEnd",
+    defaultValue: 99999,
+    // Will be set to actual buffer duration when loaded
+    minValue: 0,
+    maxValue: 99999,
+    automationRate: "k-rate"
+  },
+  startPoint: {
+    name: "startPoint",
+    defaultValue: 0,
+    minValue: 0,
+    maxValue: 9999,
+    // Max sample length in seconds
+    automationRate: "k-rate"
+  },
+  endPoint: {
+    name: "endPoint",
+    defaultValue: 9999,
+    // Will be set to actual buffer duration when loaded
+    minValue: 0,
+    maxValue: 9999,
+    automationRate: "k-rate"
+  },
+  playbackPosition: {
+    name: "playbackPosition",
+    defaultValue: 0,
+    minValue: 0,
+    maxValue: 99999,
+    automationRate: "k-rate"
+  },
+  loopDurationDriftAmount: {
+    name: "loopDurationDriftAmount",
+    defaultValue: 0,
+    minValue: 0,
+    maxValue: 1,
+    // 0 = no drift, 1 = max drift (up to 100% of loop duration)
+    automationRate: "k-rate"
+  },
+  maxLoopCount: {
+    name: "maxLoopCount",
+    defaultValue: 999999,
+    minValue: 1,
+    maxValue: 999999,
+    automationRate: "k-rate"
+  },
+  tempo: {
+    name: "tempo",
+    defaultValue: 120,
+    minValue: 20,
+    maxValue: 300,
+    automationRate: "k-rate"
+  }
+};
+const SAMPLE_PLAYER_WORKLET_AUDIOPARAM_DESCRIPTORS = Object.values(
+  SAMPLE_PLAYER_WORKLET_AUDIOPARAMS
+);
 class SamplePlayerProcessor extends AudioWorkletProcessor {
   // ===== CONSTRUCTOR =====
   constructor() {
@@ -77,107 +181,7 @@ class SamplePlayerProcessor extends AudioWorkletProcessor {
   }
   // ===== PARAMETER DESCRIPTORS =====
   static get parameterDescriptors() {
-    return [
-      {
-        name: "masterGain",
-        defaultValue: 1,
-        minValue: 0,
-        maxValue: 2,
-        automationRate: "k-rate"
-      },
-      {
-        name: "envGain",
-        defaultValue: 0,
-        minValue: 0,
-        maxValue: 1,
-        automationRate: "a-rate"
-      },
-      {
-        name: "velocity",
-        defaultValue: 100,
-        minValue: 0,
-        maxValue: 127,
-        automationRate: "k-rate"
-      },
-      {
-        name: "pan",
-        defaultValue: 0,
-        minValue: -1,
-        // -1 hard left
-        maxValue: 1,
-        // 1 hard right
-        automationRate: "k-rate"
-      },
-      {
-        name: "playbackRate",
-        defaultValue: 1,
-        minValue: 0.1,
-        maxValue: 24,
-        automationRate: "a-rate"
-      },
-      // NOTE: Time based params use seconds
-      {
-        name: "loopStart",
-        defaultValue: 0,
-        minValue: 0,
-        maxValue: 99999,
-        // Max sample length in seconds
-        automationRate: "k-rate"
-      },
-      {
-        name: "loopEnd",
-        defaultValue: 99999,
-        // Will be set to actual buffer duration when loaded
-        minValue: 0,
-        maxValue: 99999,
-        automationRate: "k-rate"
-      },
-      {
-        name: "startPoint",
-        defaultValue: 0,
-        minValue: 0,
-        maxValue: 9999,
-        // Max sample length in seconds
-        automationRate: "k-rate"
-      },
-      {
-        name: "endPoint",
-        defaultValue: 9999,
-        // Will be set to actual buffer duration when loaded
-        minValue: 0,
-        maxValue: 9999,
-        automationRate: "k-rate"
-      },
-      {
-        name: "playbackPosition",
-        defaultValue: 0,
-        minValue: 0,
-        maxValue: 99999,
-        automationRate: "k-rate"
-      },
-      {
-        name: "loopDurationDriftAmount",
-        defaultValue: 0,
-        minValue: 0,
-        maxValue: 1,
-        // 0 = no drift, 1 = max drift (up to 100% of loop duration)
-        automationRate: "k-rate"
-      },
-      {
-        name: "maxLoopCount",
-        defaultValue: 999999,
-        minValue: 1,
-        maxValue: 999999,
-        automationRate: "k-rate"
-      },
-      {
-        name: "tempo",
-        defaultValue: 120,
-        minValue: 20,
-        maxValue: 300,
-        automationRate: "k-rate"
-      }
-    ];
+    return SAMPLE_PLAYER_WORKLET_AUDIOPARAM_DESCRIPTORS;
   }
   // ===== MAIN PROCESS METHOD =====
   process(inputs, outputs, parameters) {
